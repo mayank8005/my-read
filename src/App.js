941d8eb13f856ca14c23bed11858ba4b0ededa0e
mayank8005 <mayank8005@gmail.com>
2017-12-27 03:29:10 +0530

    getting new books via API

--- src/App.js
+++ src/App.js
@@ -1,58 +1,9 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookShelf from './BookShelf'
-
-
-//data
-const books = [
-  {
-    id: 1,
-    title:'To Kill a Mockingbird',
-    author:'Harper Lee',
-    imageLinks:{
-      thumbnail: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
-    },
-    shelf:'currentlyReading'
-  },
-  {
-    id: 2,
-    title:"Ender's Game",
-    author:'Orson Scott Card',
-    imageLinks:{
-      thumbnail: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
-    },
-    shelf:'currentlyReading'
-  },
-  {
-    id: 3,
-    title:'1776',
-    author:'David McCullough',
-    imageLinks:{
-      thumbnail:"http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"
-    },
-    shelf:'wantToRead'
-  },
-  {
-    id: 4,
-    title:"Harry Potter and the Sorcerer's Stone",
-    author:'J.K. Rowling',
-    imageLinks:{
-      thumbnail:"http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"
-    },
-    shelf:'wantToRead'
-  },
-  {
-    id: 5,
-    title:"Oh, the Places You'll Go!",
-    author:'Seuss',
-    imageLinks:{
-      thumbnail:"http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api"
-    },
-    shelf:'read'
-  }
-
-];
+import Search from './Search'
+import Header from './Header'
 
 class BooksApp extends React.Component {
   state = {
@@ -62,36 +13,43 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: false,
+    books: []
+  }
+
+  changeShelf = (bookID, newShelf)=>{
+    this.setState((state)=>{
+      return {
+        books: state.books.map((book)=>{
+          if(book.id===bookID){
+            book.shelf = newShelf;
+            BooksAPI.update({id:book.id}, newShelf);
+            return book;
+          }
+          return book;
+        })
+      }
+    })
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll().then(books=>(
+      this.setState({books})
+    ))
   }
 
   render() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <Search/>
         ) : (
           <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
+            <Header/>
             <div className="list-books-content">
-              <div>
-                <BookShelf books={books.filter(book=>book.shelf==='currentlyReading')} shelfName='Currently Reading'/>
-                <BookShelf books={books.filter(book=>book.shelf==='wantToRead')} shelfName='Want To Read' />
-                <BookShelf books={books.filter(book=>book.shelf==='read')} shelfName='Read'/>
-              </div>
+                <BookShelf books={this.state.books.filter(book=>book.shelf==='currentlyReading')} shelfName='Currently Reading' changeShelf={this.changeShelf}/>
+                <BookShelf books={this.state.books.filter(book=>book.shelf==='wantToRead')} shelfName='Want To Read' changeShelf={this.changeShelf}/>
+                <BookShelf books={this.state.books.filter(book=>book.shelf==='read')} shelfName='Read'changeShelf={this.changeShelf}/>
             </div>
             <div className="open-search">
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -24,7 +24,7 @@ class BookShelf extends Component{
                                 backgroundImage: `url("${book.imageLinks.thumbnail}")` }}>
                             </div>
                             <div className="book-shelf-changer">
-                              <select>
+                              <select value={book.shelf} onChange={(event)=>this.props.changeShelf(book.id, event.target.value)}>
                                 <option value="none" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
