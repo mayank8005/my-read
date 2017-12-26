6b0423668efc0b48f09ffeaef50fd47e30b8f479
mayank8005 <mayank8005@gmail.com>
2017-12-27 03:28:34 +0530

    divided app.js into more component

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+// will return header of page
+function Header(){
+    return(
+        <div className="list-books-title">
+              <h1>MyReads</h1>
+        </div>
+    );
+}
+
+export default Header;
